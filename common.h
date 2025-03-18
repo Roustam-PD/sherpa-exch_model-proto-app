36fdc365c41d548c3cb435fae1ea5791f3f876f5
Roustam Akhmetzyanov | PASS-Dynamics <r.akhmetzyanov@pass-dynamics.com>
2025-03-18 14:42:05 +0100

    Added Platform and Control as submodules

---
+++ common.h
@@ -0,0 +1,6 @@
+﻿#ifndef COMMON_H    // Check if this file has already been included
+#define COMMON_H    // Define the macro to prevent multiple inclusions
+
+// Header file content here
+
+#endif // MY_HEADER_H  // End of include guard
